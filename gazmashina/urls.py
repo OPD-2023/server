b1d135cf6b8d9de3b45816c465326d259f512722
Vladimir <i.am.polshin@gmail.com>
2023-05-16 23:50:45 +0300

    Изменил ручку для добавления услуг

--- gazmashina/urls.py
+++ gazmashina/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from server.views import ProductList, ProductDetail, PartnerList, PartnerDetail, DirectionList, DirectionDetail, \
-    ProjectList, ProjectDetail, ServiceList, ServiceDetail, HistoryList, HistoryDetail
+    ServiceList, ServiceDetail, HistoryList, HistoryDetail, SubServiceList, SubServiceDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,10 +28,10 @@ urlpatterns = [
     path('partners/<int:pk>/', PartnerDetail.as_view(), name='partner_detail'),
     path('directions/', DirectionList.as_view(), name='direction_list'),
     path('directions/<int:pk>/', DirectionDetail.as_view(), name='direction_detail'),
-    path('projects/', ProjectList.as_view(), name='project_list'),
-    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project_detail'),
     path('services/', ServiceList.as_view(), name='service_list'),
     path('services/<int:pk>/', ServiceDetail.as_view(), name='service_detail'),
+    path('sub_services/', SubServiceList.as_view(), name='sub_service_list'),
+    path('sub_services/<int:pk>/', SubServiceDetail.as_view(), name='sub_service_detail'),
     path('history/', HistoryList.as_view(), name='history_list'),
     path('history/<int:pk>/', HistoryDetail.as_view(), name='history_detail')
 ]
--- server/urls.py
+++ server/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from server.views import ProductList, ProductDetail, PartnerList, PartnerDetail, DirectionList, DirectionDetail, \
-    ProjectList, ProjectDetail, ServiceDetail, ServiceList, HistoryList, HistoryDetail
+    ServiceDetail, ServiceList, HistoryList, HistoryDetail, SubServiceList, SubServiceDetail
 
 urlpatterns = [
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
@@ -13,10 +13,10 @@ urlpatterns = [
     path('partners/<int:pk>/', PartnerDetail.as_view(), name='partner_detail'),
     path('directions/', DirectionList.as_view(), name='direction_list'),
     path('directions/<int:pk>/', DirectionDetail.as_view(), name='direction_detail'),
-    path('projects/', ProjectList.as_view(), name='project_list'),
-    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project_detail'),
     path('services/', ServiceList.as_view(), name='service_list'),
     path('services/<int:pk>/', ServiceDetail.as_view(), name='service_detail'),
+    path('sub_services/', SubServiceList.as_view(), name='sub_service_list'),
+    path('sub_services/<int:pk>/', SubServiceDetail.as_view(), name='sub_service_detail'),
     path('history/', HistoryList.as_view(), name='history_list'),
     path('history/<int:pk>/', HistoryDetail.as_view(), name='history_detail')
 ]
